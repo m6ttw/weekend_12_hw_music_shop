2369203e3121d2ce5c3efddd93feff0721218c01
m6ttw <matthewrhyswilliams@live.com>
2021-02-19 11:59:47 +0000

    clarinet tests pass

--- src/main/java/Clarinet.java
+++ src/main/java/Clarinet.java
@@ -1,2 +1,35 @@
-public class Clarinet {
+import behaviours.IPlay;
+
+public class Clarinet implements IPlay {
+    private String make;
+    private String model;
+    private String typeOfWood;
+    private String key;
+
+    public Clarinet(String make, String model, String typeOfWood, String key) {
+        this.make = make;
+        this.model = model;
+        this.typeOfWood = typeOfWood;
+        this.key = key;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getTypeOfWood() {
+        return typeOfWood;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String play(String instrument){
+        return instrument + " is being played. ~Woo woo~";
+    }
 }
---
+++ src/test/java/ClarinetTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClarinetTest {
+    Clarinet clarinet;
+
+    @Before
+    public void before(){
+        clarinet = new Clarinet("Buffet Crampon", "R13", "Grenadilla", "Bb");
+    }
+
+    @Test
+    public void hasMake(){
+        assertEquals("Buffet Crampon", clarinet.getMake());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("R13", clarinet.getModel());
+    }
+
+    @Test
+    public void hasTypeOfWood(){
+        assertEquals("Grenadilla", clarinet.getTypeOfWood());
+    }
+
+    @Test
+    public void hasKey(){
+        assertEquals("Bb", clarinet.getKey());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Clarinet is being played. ~Woo woo~", clarinet.play("Clarinet"));
+    }
+}
